d222ae7f3c01268ef7b0b7b678984bba2213c646
Akhil Singla <akhilsingla52@gmail.com>
2022-07-03 21:03:41 +0530

    First commit

---
+++ shopbill-services/src/main/java/com/shop/bill/ShopBillApplication.java
@@ -0,0 +1,13 @@
+package com.shop.bill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopBillApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopBillApplication.class, args);
+	}
+
+}
